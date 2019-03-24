da5085fc7b986719215448ded7277bc1628b81b6
Anna Ross <anna.ross@chasacademy.se>
2019-03-24 18:17:23 +0100

    feat: Add general text components

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Typography.module.scss';
+
+export const H1 = ({ children, ...props }) => (
+  <h1 className={styles.heading1} {...props}>
+    {children}
+  </h1>
+);
+
+export const H2 = ({ children, ...props }) => (
+  <h2 className={styles.heading2} {...props}>
+    {children}
+  </h2>
+);
+
+export const InfoLabelText = ({ children, ...props }) => (
+  <p className={styles.infoLabelText} {...props}>
+    {children}
+  </p>
+);
