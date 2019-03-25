22e31eb9c14c8f11d8ad615a4b1b92a0d63aea00
Anna Ross <anna.ross@chasacademy.se>
2019-03-25 13:34:49 +0100

    feat: Add whitespace to list view

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import { Router } from '@reach/router';
 import Restaurants from './views/Restaurants/Restaurants';
 import Restaurant from './views/Restaurant/Restaurant';
 import { H1 } from './components/Typography/Typography';
+import Wrapper from './components/Wrapper/Wrapper';
 
 const App = () => {
   return (
-    <div
-      style={{
-        padding: '32px',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
+    <Wrapper>
       <H1>LunchBot</H1>
       <Router>
         <Restaurants exact path="/" />
         <Restaurant path="/restaurant/:id" />
       </Router>
-    </div>
+    </Wrapper>
   );
 };
 
---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Wrapper.module.scss';
+
+const Wrapper = (props) => <div className={styles.wrap} {...props} />;
+
+export default Wrapper;
