7b6987805907e5477ae01fc8e456cb0b1581eec7
Anna Ross <anna.ross@chasacademy.se>
2019-03-25 22:49:56 +0100

    feat: Add link to list view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from '@reach/router';
+import { Router, Link } from '@reach/router';
 import Restaurants from './views/Restaurants/Restaurants';
 import Restaurant from './views/Restaurant/Restaurant';
 import { H1 } from './components/Typography/Typography';
@@ -8,7 +8,9 @@ import Wrapper from './components/Wrapper/Wrapper';
 const App = () => {
   return (
     <Wrapper>
-      <H1>LunchBot</H1>
+      <Link to="/">
+        <H1>LunchBot</H1>
+      </Link>
       <Router>
         <Restaurants exact path="/" />
         <Restaurant path="/restaurant/:id" />
